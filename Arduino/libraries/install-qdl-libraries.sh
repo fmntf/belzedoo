bf6482597c4a4986183c303e6662153560cd24d6
Francesco Montefoschi <francesco.monte@gmail.com>
2017-11-09 14:37:34 +0000

    Libraries refactoring

--- Arduino/libraries/install-qdl-libraries.sh
+++ Arduino/libraries/install-qdl-libraries.sh
@@ -2,6 +2,7 @@
 mkdir -p ~/Arduino/libraries
 cp -R Adafruit_TCS34725 ~/Arduino/libraries
 cp -R ArduinoJson ~/Arduino/libraries
-cp -R ChainableLED ~/Arduino/libraries
-cp -R MPL3115A2_neo  ~/Arduino/libraries
 cp -R StandardCplusplus ~/Arduino/libraries
+cp -R ChainableLED ~/Arduino/libraries
+cp -R SI7021-qdl ~/Arduino/libraries
+cp -R USBHost ~/Arduino/libraries
